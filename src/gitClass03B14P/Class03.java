6d5970b0acc612aaaa32cbcff515ce97c56ededc
minahenen <w.minahanna@gmail.com>
2022-09-27 19:51:46 -0400

    second commit

--- src/gitClass03B14P/Class03.java
+++ src/gitClass03B14P/Class03.java
@@ -2,7 +2,8 @@ package gitClass03B14P;
 
 public class Class03 {
 	public static void main(String[] args) {
-		System.out.println("Hey this is a git vlass");
+		System.out.println("Hey this is a git class");
+		System.out.println("this is a change in the project");
 	}
 
 }
