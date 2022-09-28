2e7adb80098c733dfc5313f16575fed567e7033e
minahenen <w.minahanna@gmail.com>
2022-09-27 20:07:09 -0400

    third commit

--- src/gitClass03B14P/Class03.java
+++ src/gitClass03B14P/Class03.java
@@ -4,6 +4,7 @@ public class Class03 {
 	public static void main(String[] args) {
 		System.out.println("Hey this is a git class");
 		System.out.println("this is a change in the project");
+		System.out.println("Third change on the project");
 	}
 
 }
